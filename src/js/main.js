41bda90a1f7ee9449eeea8b7a8d5037c762bb526
apsulabh <supatel0618@gmail.com>
2025-09-22 14:54:24 -0500

    added resizing navbar

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,15 @@
 /* Your JS here. */
-console.log('Hello World!')
+
+let navbar = document.querySelector(".navbar");
+let isShrunk = false;
+
+window.addEventListener("scroll", () => {
+    let y = window.scrollY;
+    if (!isShrunk && y > 80) {
+      navbar.classList.add('shrink');
+      isShrunk = true;
+    } else if (isShrunk && y < 20) {
+      navbar.classList.remove('shrink');
+      isShrunk = false;
+    }
+});
