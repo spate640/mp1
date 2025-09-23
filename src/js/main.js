3aa9b121156a85cf8263a94f1ff1932d70140c56
apsulabh <supatel0618@gmail.com>
2025-09-22 22:43:22 -0500

    added position indicator, smooth scrolling, fixed background image, and working on carousel sliding window

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
 /* Your JS here. */
 
+// Shrinking navbar
 let navbar = document.querySelector(".navbar");
 let isShrunk = false;
 
@@ -12,4 +13,39 @@ window.addEventListener("scroll", () => {
       navbar.classList.remove('shrink');
       isShrunk = false;
     }
+
+    updateActiveSection();
 });
+
+
+// Dark outline when in section
+const navbarLinks = Array.from(document.querySelectorAll(".navbar a"));
+const sections = navbarLinks
+    .map(a => document.querySelector(a.getAttribute("href")));
+
+function updateActiveSection() {
+    const navBottom = navbar.getBoundingClientRect().bottom;
+
+    let currentSect = sections.find(sect => {
+        let box = sect.getBoundingClientRect();
+        return box.top <= navBottom && box.bottom > navBottom;
+    });
+
+    navbarLinks.forEach(link => {
+        link.classList.remove("active");
+    });
+
+    if(currentSect) {
+        let sectId = '#' + currentSect.id;
+        navbarLinks.forEach(link => {
+            if(link.getAttribute("href") === sectId)
+                link.classList.add("active");
+        })
+    }
+}
+
+    // Run on first load
+window.addEventListener("DOMContentLoaded", updateActiveSection);
+
+
+
